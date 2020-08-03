89b2c8915c4b4f2313737303f84d46dc2935ac84
enpitsuLin <lldslm@vip.qq.com>
2020-08-03 21:34:36 +0800

    添加了测试用例

--- testdatabase.js
+++ testdatabase.js
@@ -2,11 +2,14 @@
     "book_id": 2020001001,
     "title": "人间失格",
     "author": [
-        "太宰治"
+        "太宰治",
+        "杨伟"
     ],
     "publish": "现代文艺出版社",
-    "price": 40.8,
-    "isbn":"9787538763508"
+    "price": 48,
+    "isbn": "9787538763508",
+    "class": "小说",
+    "language": "zh-cn"
 },
 {
     "book_id": 2020001002,
@@ -14,10 +17,10 @@
     "author": [
         "罗贯中"
     ],
-    "publish": "四川出版社",
-    "price": 30,
-    "class": "文学类",
-    "isbn": "978722011478601",
+    "publish": "人民文学出版社",
+    "price": 39.5,
+    "class": "小说",
+    "isbn": "9787020008728",
     "language": "zh-cn"
 },
 {
@@ -26,9 +29,109 @@
     "author": [
         "罗贯中"
     ],
-    "publish": "四川出版社",
-    "price": 30,
-    "class": "文学类",
-    "isbn": "978722011478601",
+    "publish": "人民文学出版社",
+    "price": 39.5,
+    "class": "小说",
+    "isbn": "9787020008728",
     "language": "zh-cn"
-}]
+},
+{
+    "book_id": 2020001004,
+    "title": "红楼梦(上)",
+    "author": [
+        "曹雪芹"
+    ],
+    "publish": "人民文学出版社",
+    "price": 59.7,
+    "class": "小说",
+    "isbn": "9787020002207",
+    "language": "zh-cn"
+},
+{
+    "book_id": 2020001005,
+    "title": "红楼梦(下)",
+    "author": [
+        "曹雪芹"
+    ],
+    "publish": "人民文学出版社",
+    "price": 59.7,
+    "class": "小说",
+    "isbn": "9787020002207",
+    "language": "zh-cn"
+},
+{
+    "book_id": 2020001006,
+    "title": "水浒传(上)",
+    "author": [
+        "罗贯中",
+        "施耐庵"
+    ],
+    "publish": "人民文学出版社",
+    "price": 59.7,
+    "class": "小说",
+    "isbn": "9787020150434",
+    "language": "zh-cn"
+},
+{
+    "book_id": 2020001007,
+    "title": "水浒传(中)",
+    "author": [
+        "罗贯中",
+        "施耐庵"
+    ],
+    "publish": "人民文学出版社",
+    "price": 50.6,
+    "class": "小说",
+    "isbn": "9787020150434",
+    "language": "zh-cn"
+},
+{
+    "book_id": 2020001008,
+    "title": "水浒传(下)",
+    "author": [
+        "罗贯中",
+        "施耐庵"
+    ],
+    "publish": "人民文学出版社",
+    "price": 50.6,
+    "class": "小说",
+    "isbn": "9787020150434",
+    "language": "zh-cn"
+},
+{
+    "book_id": 2020001009,
+    "title": "西游记(上)",
+    "author": [
+        "吴承恩"
+    ],
+    "publish": "人民文学出版社",
+    "price": 96,
+    "class": "小说",
+    "isbn": "9787020150434",
+    "language": "zh-cn"
+},
+{
+    "book_id": 2020001010,
+    "title": "西游记(中)",
+    "author": [
+        "吴承恩"
+    ],
+    "publish": "人民文学出版社",
+    "price": 96,
+    "class": "小说",
+    "isbn": "9787020150434",
+    "language": "zh-cn"
+},
+{
+    "book_id": 2020001011,
+    "title": "西游记(下)",
+    "author": [
+        "吴承恩"
+    ],
+    "publish": "人民文学出版社",
+    "price": 96,
+    "class": "小说",
+    "isbn": "9787020150434",
+    "language": "zh-cn"
+}
+]
